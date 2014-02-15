44ce2e30d8a70a5147db5e62e26fa6d59cd7b2f4
varren1010 <varren1010@gmail.com>
2014-02-15 22:53:25 +0000

    Adding LST maker stuff

---
+++ Python LST Maker/Version 1 - Released/readme.txt
@@ -0,0 +1,3 @@
+To run the script, right click and "open". 
+You need Python version 3.x, as python 2.x will not recognise some of the code used and will not work.
+Answer questions using y/n or yes/no
---
+++ Python LST Maker/Version 1.1/lstmaker.py
@@ -0,0 +1,37 @@
+print ("LST Maker 1.1 Unreleased - Varren ")
+print("Press 1 to create an aircraft .lst")
+print("Press 2 to create a scenery .lst")
+menu = input(int("Enter 1 or 2"))
+
+if menu == 1:
+            addon = input("Enter a name for your files and user folder ")
+            lstline = ("/user/"+addon+"/"+addon+'.dat'+' '+"/user/"+addon+"/"+addon+'.dnm'+' '+"/user/"+addon+"/"+addon+'_coll'+'.srf'+" "+"/user/"+addon+"/"+addon+'_cock'+'.srf')
+            print (lstline)
+            ver = input("Is the information correct? ")
+            if ver == 'yes' or ver == 'Yes' or ver == 'y' or ver == 'Y':
+                fo = open("air_lstmaker.lst", "wb")
+                fo.write(bytes(lstline, 'UTF-8')) ;
+            else:
+                print ("That line will not be added")
+            status = 'complete'
+            while status == 'complete':
+                again = input("Would you like to add another line? ")
+            if again == 'yes' or again == 'Yes' or again == 'Y' or again == 'y':
+                addon2 = input("Enter another name for your files, user folder will stay the same ")
+                lstline2 = (" \r\n/user/"+addon+"/"+addon2+'.dat'+' '+"/user/"+addon+"/"+addon2+'.dnm'+' '+"/user/"+addon+"/"+addon2+'_coll'+'.srf'+" "+"/user/"+addon+"/"+addon2+'_cock'+'.srf')
+                print(lstline2)
+                ver = input("Is the information correct? ")
+                if ver == 'yes' or ver == 'Yes' or ver == 'y' or ver == 'Y':
+                    fo = open("air_lstmaker.lst", "ab")
+                    fo.write(bytes(lstline2, 'UTF-8')) ;
+            else:
+                print ("That line will not be added")
+            elif again == 'no' or again == 'No' or again == 'n' or again == 'N':
+                print("The script will now end")
+                break
+if menu == 2:
+            addon = input("Enter a name for your files and user folder ")
+
+            
+
+
